6f8e69bbd33581418453f6df5b55d0f47ead5588
mlima95 <milan.lima@hotmail.fr>
2022-06-08 15:08:11 +0200

    scrapper in progress

--- script.js
+++ script.js
@@ -3,18 +3,13 @@ const puppeteer = require("puppeteer");
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('https://www.just-eat.fr/');
-    console.log(await page.$("body"));
-    // const bodyHandle = await page.$('body');
-    // // const html = await page.evaluate((body) => body.innerHTML, bodyHandle);
-    // const [response] = await Promise.all([
-    //     page.waitForNavigation(), // The promise resolves after navigation has finished
-    //     page.evaluate((body) => body.innerHTML, bodyHandle), // Clicking the link will indirectly cause a navigation
-    // ]);
-    // console.log(response);
-    // const bodyHandle = await page.$('body');
-    // const html = await page.evaluate((body) => body.innerHTML, bodyHandle);
-    // // console.log(html);
-    // // await bodyHandle.dispose();
-    // await browser.close();
+    await page.goto('https://deliveroo.fr/');
+    // const text = await page.$('#location-search');
+    await page.type('#location-search','274 Rue de Vaugirard, 75015 Paris, France', {delay: 100});
+    const [response] = await Promise.all([
+        page.waitForNavigation(),
+        page.click('.ccl-d0484b0360a2b432.ccl-233931c277401e86.ccl-ed9aadeaa18a9f19.ccl-a97a150ddadaa172'),
+    ]);
+    console.log(response);
+    await browser.close();
 })();
